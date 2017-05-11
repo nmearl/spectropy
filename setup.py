b12d10f377ac46791a2bd6e2fbd74d9b52714cad
nmearl <nchlsearl@gmail.com>
2017-05-11 12:56:36 -0400

    Add license and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='spectropy',
+    version='0.1',
+    packages=['spectropy'],
+    url='http://github.com/nmearl/spectropy',
+    license='MIT',
+    author='Nicholas Earl',
+    author_email='contact@nicholasearl.me',
+    description='Spectropy tools in Python'
+)
